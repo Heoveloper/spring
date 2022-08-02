c4ca6f4094c69b92092ddbd876e85e7842cbacaf
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-02 11:23:45 +0900

    re

---
+++ proj1/src/com/jh/practice1/voca/Voca.java
@@ -0,0 +1,15 @@
+package com.jh.practice1.voca;
+
+public class Voca {
+    private String word;
+    private String meaning;
+
+    private int deposit;
+    private int withdraw;
+
+
+    public Voca(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+}
---
+++ proj1/src/com/jh/practice1/voca/VocaMain.java
@@ -0,0 +1,7 @@
+package com.jh.practice1.voca;
+
+public class VocaMain {
+    public static void main(String[] args) {
+        System.out.println("영어단어장");
+    }
+}
