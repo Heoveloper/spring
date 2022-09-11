32dc7d15af20e508710f8cdfcb9fa619dcd096bf
Junhyeok Heo <wnsgur5508@gmail.com>
2022-09-11 21:53:33 +0900

    practice great

---
+++ great1/src/main/java/com/example/great1/Great1Application.java
@@ -0,0 +1,13 @@
+package com.example.great1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Great1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Great1Application.class, args);
+	}
+
+}
---
+++ great1/src/test/java/com/example/great1/Great1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.great1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Great1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
