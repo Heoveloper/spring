f10bee97874e258bccda71029ec9f8c38dde7741
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-09-15 10:34:23 +0900

    Rest API

--- src/main/java/com/kh/demo/web/ApiProductController.java
+++ src/main/java/com/kh/demo/web/ApiProductController.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j  //log.info()
 @Controller
@@ -25,4 +29,35 @@ public class ApiProductController {
 //    public void setInstance (ProductSVC productSVC) {
 //    this.productSVC = productSVC;
 //    }
+
+    //등록 POST /api/products
+    @PostMapping("/products")
+    public String add() {
+        return "ok";
+    }
+
+    //조회 GET /api/products/{id}
+    @GetMapping("/products/{id}")
+    public String findById() {
+        return "ok";
+    }
+
+    //수정 PATCH /api/products/{id}
+    @PatchMapping("/products/{id}")
+    public String edit() {
+        return "ok";
+    }
+
+    //삭제 DELETE /api/products/{id}
+    @DeleteMapping("/products/{id}")
+    public String del() {
+        return "ok";
+    }
+
+    //목록 GET /api/products
+    @GetMapping("/products")
+    public String findAll() {
+        return "ok";
+    }
+
 }
