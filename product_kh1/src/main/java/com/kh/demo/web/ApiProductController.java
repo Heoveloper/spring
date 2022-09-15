8969a72d82c5d84ea3d5475d56488c896de9611a
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-09-15 12:47:32 +0900

    API Response

--- product_kh1/src/main/java/com/kh/demo/web/ApiProductController.java
+++ product_kh1/src/main/java/com/kh/demo/web/ApiProductController.java
@@ -1,18 +1,23 @@
 package com.kh.demo.web;
 
+import com.kh.demo.dao.Product;
 import com.kh.demo.svc.ProductSVC;
+import com.kh.demo.web.api.ApiResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j //log.info()
 @Controller
 @RequiredArgsConstructor //생성자 생성
+@RequestMapping("/api")
 public class ApiProductController {
 
     @Autowired
@@ -31,10 +36,63 @@ public class ApiProductController {
 //    }
 
     //등록 POST /api/products
-    @PostMapping("/products")
-    public String add() {
-        return "ok";
+    @ResponseBody //View를 찾지 않고 Body를 반환
+    @PostMapping(
+            value = "/products"
+            //,produces = "application/json"
+    )
+    public ApiResponse<List<Person>> add(@RequestBody String reqMsg) {
+        log.info("reqMsg={}", reqMsg);
+
+        List<Person> persons = new ArrayList<>();
+
+        Person p1 = new Person("홍길동", 30);
+        Person p2 = new Person("홍길순", 20);
+
+        persons.add(p1);
+        persons.add(p2);
+
+        ApiResponse.Header header = new ApiResponse.Header("00", "성공!");
+        ApiResponse<List<Person>> response = new ApiResponse<>(header, persons);
+
+        return response;
+
+        //Set<Person> persons = new HashSet<>();
+        //Map<String, Person> persons = new HashMap<>();
+        //List<Person> persons = new ArrayList<>();
+
+        //Person p1 = new Person("홍길동", 30);
+        //Set: persons.add(p1);
+        //Map: persons.put("1", p1);
+        //List: persons.add(p1);
+
+        //Person p2 = new Person("홍길순", 20);
+        //Set: persons.add(p2);
+        //Map: persons.put("2", p2);
+        //List: persons.add(p2);
     }
+
+    @Data
+    @AllArgsConstructor
+    static class Person {
+        private String name;
+        private int age;
+    }
+
+    //제너릭 타입을 사용해서 사용시점에 타입을 결정
+//    @Data
+//    @AllArgsConstructor
+//    static class Response<T> {
+//        private Header header;
+//        private T data;
+//
+//        @Data
+//        @AllArgsConstructor
+//        static class Header {
+//            private String rtcd;
+//            private String rtmsg;
+//        }
+//    }
 
     //조회 GET /api/products/{id}
     @GetMapping("/products/{id}")
@@ -55,9 +113,17 @@ public class ApiProductController {
     }
 
     //목록 GET /api/products
+    @ResponseBody
     @GetMapping("/products")
-    public String findAll() {
-        return "ok";
+    public ApiResponse<List<Product>> findAll() {
+
+        List<Product> list = productSVC.findAll();
+
+        //api 응답메세지
+        ApiResponse.Header header = new ApiResponse.Header("00", "성공!");
+        ApiResponse<List<Product>> res = new ApiResponse<>(header, list);
+
+        return res;
     }
 
 }
