05296cd65dc3c50802fd1afcbde1d84822284c37
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-09-15 10:21:04 +0900

    Rest API

--- src/main/java/com/kh/demo/web/ApiProductController.java
+++ src/main/java/com/kh/demo/web/ApiProductController.java
@@ -2,14 +2,25 @@ package com.kh.demo.web;
 
 import com.kh.demo.svc.ProductSVC;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 @Slf4j  //log.info()
+@Controller
 public class ApiProductController {
 
-    private final ProductSVC productSVC;
+    @Autowired
+    private ProductSVC productSVC;
 
     //생성자 주입
+    @Autowired
     public ApiProductController(ProductSVC productSVC) {
         this.productSVC = productSVC;
+    }
+
+    //세터메소드 주입
+    @Autowired
+    public void setInstance (ProductSVC productSVC) {
+    this.productSVC = productSVC;
     }
 }
