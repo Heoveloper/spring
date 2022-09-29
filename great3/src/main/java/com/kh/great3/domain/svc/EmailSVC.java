797e9c86410b8a84e907c7733f7c3780f0445be9
Junhyeok Heo <wnsgur5508@gmail.com>
2022-09-29 23:32:40 +0900

    delete great3, add great

--- great3/src/main/java/com/kh/great3/domain/svc/EmailSVC.java
+++
@@ -1,7 +0,0 @@
-//package com.kh.great3.domain.svc;
-//
-//public interface EmailSVC {
-//    public String sendForgotPassword(String memId, String memEmail);
-//
-//    public String getTempPassword();
-//}
